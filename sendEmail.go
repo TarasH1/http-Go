049f0f9270b9f9c7566a4186f352caa41c676266
Taras H <research.010@gmail.com>
2019-11-22 17:11:06 +0200

    test mail and template

--- sendEmail.go
+++ sendEmail.go
@@ -4,27 +4,84 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"net/smtp"
 )
 
 var auth smtp.Auth
 
+func queryParams(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		for k, v := range r.URL.Query() {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+		w.Write([]byte("Received a GET request\n"))
+	case "POST":
+		reqBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", reqBody)
+		w.Write([]byte("Received a POST request\n"))
+
+		emails, ok := r.URL.Query()["email"]
+
+		if !ok || len(emails[0]) < 1 {
+			log.Println("URL Param 'email' is missing")
+			return
+		}
+		email := emails[0]
+		log.Println("URL Param 'email' is: " + string(email))
+
+		for k, v := range r.URL.Query() {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+		auth = smtp.PlainAuth("", "taras.h.ua@gmail.com", "cakeslice", "smtp.gmail.com")
+		templateData := struct {
+			Name string
+			URL  string
+		}{
+			Name: "User",
+			URL:  "http://wisehands.me/book.pdf",
+		}
+		r := NewRequest([]string{"research010@gmail.com"}, "Hello User", "Hello")
+		if err := r.ParseTemplate("template.html", templateData); err == nil {
+			ok, _ := r.SendEmail()
+			fmt.Println(ok)
+		}
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+	}
+}
+
 func main() {
-	auth = smtp.PlainAuth("", "taras.h.ua@gmail.com", "cakeslice", "smtp.gmail.com")
+	/*	auth = smtp.PlainAuth("", "taras.h.ua@gmail.com", "cakeslice", "smtp.gmail.com")
 	templateData := struct {
 		Name string
 		URL  string
 	}{
 		Name: "User",
 		URL:  "http://wisehands.me/book.pdf",
-	}
-	r := NewRequest([]string{"research010@gmail.com"}, "Hello User!", "Hello, World!")
+	}*/
+	/*	r := NewRequest([]string{"research010@gmail.com"}, "Hello User", "Hello")
 	if err := r.ParseTemplate("template.html", templateData); err == nil {
 		ok, _ := r.SendEmail()
 		fmt.Println(ok)
-	}
+	}*/
 
+	http.HandleFunc("/", queryParams)
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8000", nil)
 }
+
 
 //Request struct
 type Request struct {
@@ -48,7 +105,7 @@ func (r *Request) SendEmail() (bool, error) {
 	msg := []byte(subject + mime + "\n" + r.body)
 	addr := "smtp.gmail.com:587"
 
-	if err := smtp.SendMail(addr, auth, "dhanush@geektrust.in", r.to, msg); err != nil {
+	if err := smtp.SendMail(addr, auth, "taras.h.ua@gmail.com", r.to, msg); err != nil {
 		return false, err
 	}
 	return true, nil
