a145bdc87c1461bb7f0c65d4230405163eb44433
Taras H <research.010@gmail.com>
2019-12-04 20:04:18 +0200

    email fixes

--- httpGo.go
+++ httpGo.go
@@ -128,6 +128,7 @@ func (r *Request) SendEmail() (bool, error) {
 	addr := "smtp.gmail.com:587"
 
 	if err := smtp.SendMail(addr, auth, "taras.h.ua@gmail.com", r.to, msg); err != nil {
+		log.Fatal(err)
 		return false, err
 	}
 	return true, nil
