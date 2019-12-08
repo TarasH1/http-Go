f962f30c8bd65ff84cd746ab61748129bbf12b3c
Taras H <research.010@gmail.com>
2019-12-08 23:06:26 +0200

    order book endpoint

--- httpGo.go
+++ httpGo.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/smtp"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
@@ -51,13 +53,13 @@ func queryParams(w http.ResponseWriter, r *http.Request) {
 		for k, v := range r.URL.Query() {
 			fmt.Printf("%s: %s\n", k, v)
 		}
-        //Mail authorization
+		//Mail authorization
 		auth = smtp.PlainAuth("", "taras.h.ua@gmail.com", "mlxqtvziciulbigo", "smtp.gmail.com")
 
 		templateData := struct {
-			URL  string
+			URL string
 		}{
-			URL:  "https://three-sides.com/pdf/tsoh.pdf",
+			URL: "https://three-sides.com/pdf/tsoh.pdf",
 		}
 		r := NewRequest([]string{email}, "Ваша електронна копія книги", "")
 		if err := r.ParseTemplate("mailTemplate.html", templateData); err == nil {
@@ -80,14 +82,14 @@ func receiveData(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%s\n", reqBody)
 		w.Write([]byte("Received a POST request\n"))
 
-/*		emails, ok := r.URL.Query()["email"]
+		/*		emails, ok := r.URL.Query()["email"]
 
-		if !ok || len(emails[0]) < 1 {
-			log.Println("URL Param 'email' is missing")
-			return
-		}
-		email := emails[0]
-		log.Println("URL Param 'email' is: " + email)*/
+				if !ok || len(emails[0]) < 1 {
+					log.Println("URL Param 'email' is missing")
+					return
+				}
+				email := emails[0]
+				log.Println("URL Param 'email' is: " + email)*/
 
 		//Display all request params
 		for k, v := range r.URL.Query() {
@@ -99,15 +101,35 @@ func receiveData(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func sendData(w http.ResponseWriter, r *http.Request) {
+	formData := url.Values{
+		"name": {"john_doe"},
+	}
+
+	resp, err := http.PostForm("https://wayforpay.com/post", formData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result["form"])
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("../book"))
 	http.Handle("/", http.StripPrefix("/", fs))
 
+	//HTTP server endpoints
 	http.HandleFunc("/api", queryParams)
 	http.HandleFunc("/api/payment/done", receiveData)
 	http.HandleFunc("/success", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Success Page")
 	})
+	http.HandleFunc("/order-book", sendData)
+
 	port := ":5446"
 	reloadable()
 	fmt.Println("Server is listening... on port" + port)
